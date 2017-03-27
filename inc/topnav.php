9b6bd239f716047df7aaf5dd58da9f282c1a872b
Jack <jackdhwork@gmail.com>
2017-03-27 12:22:50 +0100

    Initial commit

---
+++ inc/topnav.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-default bottom-nav">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <li ><a href="./">Default <span class="sr-only">(current)</span>
+                    <span class="caret"></span>
+                    </a></li>
+                <li><a href="./">Static top
+                    <span class="caret"></span>
+                    </a></li>
+                <li><a href="./">Fixed top
+                    <span class="caret"></span>
+                    </a></li>
+            </ul>
+        </div><!--/.nav-collapse -->
+    </div><!--/.container-fluid -->
+</nav>
